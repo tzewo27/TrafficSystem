bb67c8cb460a8140518a318643fece4549bb2609
Tizita Endeshaw Yilma <tizita.endeshaw@aastustudent.edu.et>
2026-05-12 00:18:09 +0300

    Clean up Main.java by removing commented instructions
    Removed commented out terminal instructions for GUI and RMI.

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,4 @@
-// for GUI
-//terminal 1
-//cd c:\Users\tizit\OneDrive\Desktop\AP-project\TrafficSystem
-//javac -cp "lib\*;src" -d bin src\Main.java src\models\*.java src\database\*.java src\network\*.java src\gui\*.java
-//java -cp "bin;lib\*" network.TrafficServer
-//terminal2
-//cd c:\Users\tizit\OneDrive\Desktop\AP-project\TrafficSystem
-//java -cp "bin;lib\*" Main
 
-
-//for RMI
-// Terminal 1 — compile everything:
-// cd c:\Users\tizit\OneDrive\Desktop\AP-project\TrafficSystem
-// javac -cp "lib\*;src" -d bin src\Main.java src\models\*.java src\database\*.java src\network\*.java src\gui\*.java
-// Terminal 2 — start RMI server:
-// java -cp "bin;lib\*" network.RMIServer
-// You should see:
-// Setting up database...
-// RMI Server started on port 1099
-// Service registered as: IncidentService
-// Waiting for remote calls...
-// Terminal 3 — start TCP server:
-// java -cp "bin;lib\*" network.TrafficServer
-// Terminal 4 — run the GUI:
-// java -cp "bin;lib\*" Main
-//http://localhost:8080
 
 import database.DatabaseConnection;
 import gui.LoginScreen;
